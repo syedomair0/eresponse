2e888929b8108ad39771f708dfc21d9de97f38d9
Mahmood Syed <syedm02@Student-syedm02.local>
2020-02-11 13:30:40 -0600

    initial commit

---
+++ src/main/java/org/psdr3/eresponse/EresponseApplication.java
@@ -0,0 +1,13 @@
+package org.psdr3.eresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EresponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EresponseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/psdr3/eresponse/EresponseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.psdr3.eresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EresponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
