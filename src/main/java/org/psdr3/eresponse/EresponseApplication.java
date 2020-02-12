54b5e8be77456e0361b96d41ed8309dab778da99
dieyem00 <dieyem00@10.61.52.74>
2020-02-12 14:12:35 -0600

    back to what it was

--- src/main/java/org/psdr3/eresponse/EresponseApplication.java
+++ src/main/java/org/psdr3/eresponse/EresponseApplication.java
@@ -3,50 +3,15 @@ package org.psdr3.eresponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import classrestservice.Person;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+
 
 @SpringBootApplication
 public class EresponseApplication {
-	public static final Logger LOGGER
-	= LoggerFactory.getLogger();
-
-public static final String CONNECTION_STRING
-= "";
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EresponseApplication.class, args);
-		
-			
-			try(Connection connection = 
-					DriverManager.getConnection(CONNECTION_STRING)){
-				String insert = "INSERT INTO checkinStudents (studentId, checkinTime " 
-						+ "VALUES (?, ?)";
-				PreparedStatement statement = connection.prepareStatement(insert);
-				statement.setString( );
-				statement.setString( );
-				
-				
-				statement.executeUpdate();
-				connection.close(); 
-
-				
-			} catch (SQLException e) {
-				LOGGER.debug(e.getMessage());
-				return null;
-			}
-		
-		
-		
 	
 	}
 
