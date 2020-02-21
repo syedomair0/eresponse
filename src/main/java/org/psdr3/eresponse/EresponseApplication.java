8510a4ad0e64c45b971747fbeaab4f68d830a1cb
dieyem00 <dieyem00@10.61.52.74>
2020-02-21 14:08:20 -0600

    commit

--- src/main/java/org/psdr3/eresponse/EresponseApplication.java
+++ src/main/java/org/psdr3/eresponse/EresponseApplication.java
@@ -2,8 +2,6 @@ package org.psdr3.eresponse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-<<<<<<< HEAD
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,19 +18,6 @@ import org.slf4j.LoggerFactory;
 public class EresponseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EresponseApplication.class, args);
-=======
-
-
-
-
-@SpringBootApplication
-public class EresponseApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(EresponseApplication.class, args);
-	
->>>>>>> 54b5e8be77456e0361b96d41ed8309dab778da99
 	}
-
 }
 
