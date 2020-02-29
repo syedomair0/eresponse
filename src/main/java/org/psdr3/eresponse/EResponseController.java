c3a2269ce52961238effb7d4ba988cd32f9b1a5e
Mahmood Syed <syedm02@Student-syedm02.local>
2020-02-29 16:46:26 -0600

    actually fixed

--- src/main/java/org/psdr3/eresponse/EResponseController.java
+++ src/main/java/org/psdr3/eresponse/EResponseController.java
@@ -5,13 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/* This is similar to the videos but with GetMapping(events)
- * 									      PostMapping(events)	
- * 										  PostMapping(events)/{event-id}/{student-id}
- */
-
-
-
 
 @RestController
 @CrossOrigin
@@ -19,26 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class EResponseController {
 
-	
-	
-	
-
 	@PostMapping(value="/events/{event_id}/{student_id}")
 	public boolean createCheckin(@PathVariable("event_id") int eventId, @PathVariable("student_id") String studentId) {
 		return EResponseDBService.createCheckin(eventId, studentId);
 	}
-	
-	
-	
-	
-	
-	
-	
-
-
-	
-
-	
-	
 	
 }
